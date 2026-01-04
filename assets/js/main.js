bf0c8eb435b0bf170302d0838fd23b43a0ef3779
vighnesh chiluka <vighnesh.chiluka25@gmail.com>
2026-01-04 21:47:01 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+
+// ---------------tab-panel-js--------------------
+
+const buttons = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".tab-content");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    // remove active from all
+    buttons.forEach(btn => btn.classList.remove("active"));
+    contents.forEach(content => content.classList.remove("active"));
+
+    // add active to clicked button + matching content
+    button.classList.add("active");
+    document.getElementById(button.dataset.tab).classList.add("active");
+  });
+});
+
+// ----------------------hamburger-menu-------------------
+
+
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
+  });
